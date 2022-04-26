2bbf2bb6eb53db93a6506249ad8a652e16382c2b
Overlander <anuragnandal@hotmail.com>
2022-04-26 13:48:06 -0400

    Add ability to create start node and its 8 children

--- lib/knights_travails.rb
+++ lib/knights_travails.rb
@@ -1,17 +1,27 @@
+require 'pry-byebug'
+
 class Board
+  attr_reader :start
+
   def initialize(start)
     @start = start
-    p @start
+    @knight = Node.new(start)
   end
 
   def possible_moves
-    moves = (0..7).to_a.permutation(2).to_a
-    (0..7).flat_map { |num| moves << [num, num] }
-    moves
+    [[1, 2], [1, -2], [-1, 2], [-1, -2], [2, 1], [2, -1], [-2, 1], [-2, -1]]
   end
 
   def create_children
-    
+    possible_moves.each do |move|
+      x_coord = start[0] + move[0]
+      y_coord = start[1] + move[1]
+      Node.new([x_coord, y_coord], @knight) if valid_move?(x_coord, y_coord)
+    end
+  end
+
+  def valid_move?(x_coord, y_coord)
+    x_coord.between?(0, 7) && y_coord.between?(0, 7)
   end
 end
 
@@ -19,5 +29,13 @@ class Knight
 
 end
 
-game = Board.new([0,0])
-p game.possible_moves.count
+class Node
+  def initialize(value, parent = nil)
+    @value = value
+    @parent = parent
+    p "#{value} parent: #{parent}"
+  end
+end
+
+game = Board.new([3, 3])
+game.create_children
